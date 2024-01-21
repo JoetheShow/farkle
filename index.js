ac554e100278aad76b63d0bdc42333040eab154b
JoeHampton <joehampton50@gmail.com>
2024-01-21 17:14:54 -0500

    Finish working game

--- index.js
+++ index.js
@@ -1,38 +1,265 @@
-$(document).ready(function() {
-  $("#add-player").click(function() {
-    $("#player-name").val() // this is the player name entered into the text field
-    var playerName = $("#player-name").val();
+class Farkle {
+  static lastScore = 0;
+  static gameStarted = false;
+  static finalRound = false;
+
+  static initialize() {
+    this.buttonFor("addPlayer").click(this.addPlayer);
+    this.buttonFor("startGame").click(this.startGame);
+    this.buttonFor("playerFarkle").click(function() {
+      Farkle.lastScore = 0;
+      Farkle.nextTurn();
+    });
+
+    this.players = [];
+  }
+
+  static play9000Sound() {
+    var audio = new Audio('over-9000.wav');
+    audio.play();
+  }
+
+  static addPlayer() {
+    let playerName = Farkle.valueFor("playerName");
 
     if(playerName == "") {
       alert("Please enter a name");
+
+      return;
+    }
+
+    Farkle.elementFor("playerName").val("");
+
+    let player = new FarklePlayer(playerName);
+    Farkle.players.push(player);
+    player.render();
+
+    if(Farkle.players.length > 1) {
+      Farkle.buttonFor("startGame").removeClass("hide");
+    }
+  }
+
+  static startGame() {
+    Farkle.elementFor("addPlayersContainer").addClass("hide");
+    $(`[data-farkle-button="playerFarkle"]`).removeClass("hide");
+    Farkle.gameStarted = true;
+
+    Farkle.players[0].turn = true;
+    Farkle.update();
+  }
+
+  static nextTurn() {
+    let currentTurn = Farkle.players.findIndex(player => player.turn);
+    let nextPlayer;
+
+    Farkle.players[currentTurn].turn = false;
+
+    if(Farkle.finalRound) {
+      Farkle.players[currentTurn].endgame = true;
+    }
+
+    if(currentTurn == Farkle.players.length - 1) {
+      nextPlayer = Farkle.players[0];
     } else {
-      $("#player-name").val("");
+      nextPlayer = Farkle.players[currentTurn + 1];
+    }
 
-      // copy the player template column
-      var playerContainer = $("#player-template").clone();
+    if(Farkle.finalRound && nextPlayer.endgame) {
+      let winner = Farkle.players.reduce((highest, player) => {
+        if(player.totalScore() > highest.totalScore()) {
+          return player;
+        } else {
+          return highest;
+        }
+      }, Farkle.players[0]);
 
-      // unhide the template
-      playerContainer.removeClass("hide");
+      alert(`Game over! ${winner.name} wins!`);
+    } else {
+      nextPlayer.turn = true;
+    }
 
-      // inject the template into the page
-      $(".players").append(playerContainer);
+    Farkle.update();
+  }
 
-      // put player's name into the template
-      playerContainer.find(".player-name").html(playerName);
+  static update() {
+    Farkle.elementFor("lastScore").html(this.lastScore);
 
-      if($(".player-container").length > 2) {
-        $("#start-button").removeClass("hide");
+    let everyoneHasScored = Farkle.players.every(player => player.scores.length > 0);
+
+    if(everyoneHasScored) {
+      Farkle.elementFor("lastScoreWrapper").removeClass("hide");
+    } else {
+      Farkle.elementFor("lastScoreWrapper").addClass("hide");
+    }
+
+    Farkle.players.forEach(player => {
+      player.update();
+    });
+  }
+
+  static buttonFor(name) {
+    return $(`[data-farkle-button="${name}"]`);
+  }
+
+  static elementFor(name) {
+    return $(`[data-farkle-element="${name}"]`);
+  }
+
+  static valueFor(name) {
+    return this.elementFor(name).val();
+  }
+}
+
+class FarklePlayer {
+  constructor(name) {
+    this.name = name;
+    this.scores = [];
+    this.container;
+    this.turn = false;
+    this.endgame = false;
+    this.crossed9000 = false;
+  }
+
+  render() {
+    this.container = FarklePlayer.template();
+    this.container.data("farkle-player", this);
+    
+    this.container.removeClass("hide");
+    this.container.removeAttr("id");
+    this.container.removeAttr("data-farkle-element");
+    this.container.find(`[data-farkle-element="playerName"]`).html(this.name);
+    this.container.find(`[data-farkle-button="addPlayerScore"]`).click(function() {
+      FarklePlayer.addScore(this);
+    });
+
+    this.container.find(`[data-farkle-button="removePlayer"]`).click(function() {
+      let confirmation = confirm("Are you sure you want to remove this player?");
+
+      if(!confirmation) {
+        return;
       }
+
+      let player = $(this).closest(".player-container").data("farkle-player");
+      let index = Farkle.players.indexOf(player);
+
+      Farkle.players.splice(index, 1);
+      player.container.remove();
+    });
+
+    Farkle.elementFor("playersContainer").append(this.container);
+  }
+
+  update() {
+    this.container.find(`[data-farkle-element="playerScore"]`).html(this.totalScore());
+
+    if(this.totalScore() >= 9000 && !this.crossed9000) {
+      this.container.find(`[data-farkle-element="playerScore"]`).addClass("red");
+      Farkle.play9000Sound();
+      this.crossed9000 = true;
+    }
+
+    if(this.endgame) {
+      this.container.find(`[data-farkle-element="scoreLabel"]`).html("Final Score:");
+    }
+
+    if(Farkle.finalRound) {
+      let highestScore = Farkle.players.reduce((highest, player) => {
+        if(player.totalScore() > highest) {
+          return player.totalScore();
+        } else {
+          return highest;
+        }
+      }, 0);
+
+      if(this.totalScore() < highestScore) {
+        let pointsNeededToWin = (highestScore - this.totalScore()) + 50;
+
+        this.container.find(`[data-farkle-element="pointsNeededToWin"]`).parent().removeClass("hide");
+        this.container.find(`[data-farkle-element="pointsNeededToWin"]`).removeClass("hide").html(pointsNeededToWin);
+      }
+    }
+
+    if(Farkle.gameStarted) {
+      this.container.find(`[data-farkle-element="turnScore"]`).removeClass("hide");
+      this.container.find(`[data-farkle-button="addPlayerScore"]`).removeClass("hide");
+      this.container.find(`[data-farkle-element="playerScore"]`).parent().removeClass("hide");
+    }
+
+    if(this.scores.length > 0) {
+      this.container.find(`[data-farkle-element="playerScoreHistoryList"]`).html("");
+      this.scores.forEach(score => {
+        this.container.find(`[data-farkle-element="playerScoreHistoryList"]`).append(`<li>${score}</li>`);
+      });
+    }
+
+    if(this.turn) {
+      this.container.addClass("active");
+      this.container.find(`[data-farkle-element="turnScore"]`).attr("disabled", false);
+      this.container.find(`[data-farkle-button="addPlayerScore"]`).attr("disabled", false);
+    } else {
+      this.container.removeClass("active");
+      this.container.find(`[data-farkle-element="turnScore"]`).attr("disabled", true);
+      this.container.find(`[data-farkle-button="addPlayerScore"]`).attr("disabled", true);
+    }
+  }
+  
+  static addScore(button) {
+    let player = $(button).closest(".player-container").data("farkle-player");
+    let score = player.container.find(`[data-farkle-element="turnScore"]`).val();
+
+    if(score == "") {
+      return;
+    }
+
+    if(score % 50 != 0) {
+      alert("Invalid score - do you even math, bro?");
+
+      return;
+    } else {
+      if(player.scores.length == 0 && score < 500) {
+        alert("First score must be at least 500 - size matters!");
+      } else {
+        player.scores.push(parseInt(score));
+        Farkle.lastScore = parseInt(score);
+
+        if(player.totalScore() >= 10000) {
+          player.endgame = true;
+          Farkle.finalRound = true;
+        }
+
+        player.container.find(`[data-farkle-element="turnScore"]`).val("");
+        Farkle.nextTurn();
+      }
+    }
+  }
+
+  totalScore() {
+    return this.scores.reduce((total, score) => total + score, 0);
+  }
+
+  static template() {
+    return $("#player-template").clone();
+  }
+}
+
+$(document).ready(function() {
+  $(".flyout .handle").click(function() {
+    $(".flyout").toggleClass("open");
+  });
+
+  $("#togglePlayerDeletion").change(function() {
+    if(this.checked) {
+      $(".remove-player").removeClass("hide");
+    } else {
+      $(".remove-player").addClass("hide");
     }
   });
 
-  $(".active-player").click(function() {
-    $(".player-container").removeClass("active");
-
-    $(this).closest(".player-container").addClass("active");
-  });
-
-  $("#start-button").click(function() {
-    $(".player-container:first").addClass("active");
+  $("#togglePlayerScoreHistory").change(function() {
+    if(this.checked) {
+      $(`[data-farkle-element="playerScoreHistory"]`).removeClass("hide");
+    } else {
+      $(`[data-farkle-element="playerScoreHistory"]`).addClass("hide");
+    }
   });
 });
